379249a0aecb45ad23b5c8d34e0c49d9a6d28aef
Francisco Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-05-21 22:24:04 +0200

    controller fixed

--- src/main/java/com/example/advance_di_postman/controllers/DiscountController.java
+++ src/main/java/com/example/advance_di_postman/controllers/DiscountController.java
@@ -4,19 +4,17 @@ import com.example.advance_di_postman.dtos.RequestDto;
 import com.example.advance_di_postman.services.EarlyBirdDiscountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/discount")
 public class DiscountController {
     @Autowired
     EarlyBirdDiscountService earlyBirdDiscountService;
 
-    @PostMapping()
+    @GetMapping
     public ResponseEntity<?> discount(@RequestBody RequestDto requestDto){
         return earlyBirdDiscountService.earlyBirdDiscount(requestDto);
+
     }
 }
