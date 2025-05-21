c15a086b2747dbed917bfbeea5143e714de9b29f
Francisco Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-05-21 15:15:35 +0200

    first commit

---
+++ src/main/java/com/example/advance_di_postman/AdvanceDiPostmanApplication.java
@@ -0,0 +1,13 @@
+package com.example.advance_di_postman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceDiPostmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvanceDiPostmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/advance_di_postman/AdvanceDiPostmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advance_di_postman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvanceDiPostmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
